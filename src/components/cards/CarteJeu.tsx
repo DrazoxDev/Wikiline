9ec1510e3c5ffd2c08d180d80bfbf799485e6d64
ThomasCHAUVIN <Thomas_Chauvin.Etu@univ-lemans.fr>
2026-06-11 08:59:31 +0200

    test

--- src/components/cards/CarteJeu.tsx
+++ src/components/cards/CarteJeu.tsx
@@ -7,7 +7,7 @@ const [isFlipped, setIsFlipped] = useState(false);
         <>
         {
 
-<div
+        <div
             className="absolute top-0 right-0 w-52 h-72 cursor-pointer"
             onClick={() => setIsFlipped(!isFlipped)}
         >
