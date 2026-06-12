cdf75923a3bb424cd63372728eafa0fa61caaf02
ThomasCHAUVIN <Thomas_Chauvin.Etu@univ-lemans.fr>
2026-06-12 11:23:24 +0200

    404

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -6,9 +6,9 @@ const Page404 = () => {
     <main className="mx-auto max-w-7xl min-h-screen">
       <section className="pb-5 grid gap-4 justify-items-center">
         <img src={img404} alt="404" className="w-full" />
-        <h1 className="text-center">Égaré sur les autoroutes de l’information</h1>
+        <h1 className="text-center">Égaré sur la TimeLine du fun</h1>
         <p className="p-2">À l’heure où nous écrivons ces lignes, nous ne savons pas ce qu’il s’est précisément passé. Nous connaissons le résultat : vous êtes arrivé ici, dans cette rue sans issue de l’Internet. Vous vous êtes égaré sur les chemins pourtant tout tracés des autoroutes de l’information. Vous souhaitez désormais retrouver un environnement plus utile : la page d’accueil semble une bonne solution pour repartir de l’avant.</p>
-        <Link to="/" className="rounded-lg text-white bg-blue-600 p-4 block max-content">Prendre un nouveau départ</Link>
+        <Link to="/" className="rounded-lg text-white bg-blue-600 p-4 block max-content">Direction WikiLine</Link>
       </section>
     </main>
   );
